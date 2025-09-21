d805b9f64fec4fe6215a0c68bea0e6d083f95b40
JulesRuby <JulesRuby@users.noreply.github.com>
2025-09-20 19:54:31 -0600

    Added some slightly different styling

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function StudentInfo() {
   return (
-    <article className="p-1rem rounded-xs shadow-md">
+    <article className="p-[1rem] rounded-xs shadow-md">
       <p className="font-bold">J.R. Muri</p>
-      <Link href="https://github.com/JulesRuby/cprg306-assignments">Checkout my github!</Link>
+      <Link href="https://github.com/JulesRuby/cprg306-assignments" className="text-teal-400">Checkout my github!</Link>
     </article>
   );
 }
