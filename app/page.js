9e041d7d99e7577c8751306da81edd3c259aaa5d
JulesRuby <JulesRuby@users.noreply.github.com>
2025-09-22 12:06:35 -0600

    Added in Link to the week-3 page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <header>
       <h1 className="text-xl text-bold">CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="http://localhost:3000/week-2" className="text-cyan-400">Week 2 - Assignment Page</Link>
+      <Link href="http://localhost:3000/week-3" className="text-cyan-400">Week 3 - Assignment Page</Link>
     </header>
   );
 }
