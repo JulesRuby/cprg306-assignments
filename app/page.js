fe4dc191957c86e572c42bc0a11c1ff2a38f4f68
JulesRuby <JulesRuby@users.noreply.github.com>
2025-09-23 16:53:34 -0600

    Added very basic nav structure

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
 import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <header>
-      <h1 className="text-xl text-bold">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="http://localhost:3000/week-2" className="text-cyan-400">Week 2 - Assignment Page</Link>
-      <Link href="http://localhost:3000/week-3" className="text-cyan-400">Week 3 - Assignment Page</Link>
-    </header>
-  );
+	return (
+		<header>
+			<h1 className='text-xl text-bold'>
+				CPRG 306: Web Development 2 - Assignments
+			</h1>
+			<nav className=''>
+				<ul className='nav-list'>
+					<li>
+						<Link href='http://localhost:3000/week-2' className='text-cyan-400'>
+							Week 2 - Assignment Page
+						</Link>
+					</li>
+					<li>
+						<Link href='http://localhost:3000/week-3' className='text-cyan-400'>
+							Week 3 - Assignment Page
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
