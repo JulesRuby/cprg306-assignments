80660c0b9c5da21bbf34a4cc831acc16bf3d9aa8
JulesRuby <JulesRuby@users.noreply.github.com>
2025-09-28 23:44:22 -0600

    Added better styling. Also added in page metadata

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,23 @@
 import items from './items.json';
 import ItemList from './item-list.js';
+// for metadata for page title and description
+import { createPageMetadata } from '../../lib/metadata.js';
+
+// using next's generateMetadata function to create dynamic metadata for the page
+export async function generateMetadata() {
+	return createPageMetadata({
+		title: 'Shopping List',
+		description:
+			'Practicing components and props by creating a shopping list app.',
+	});
+}
 
 function Page() {
 	return (
-		<main>
-			<h2 className='text-xl'>Week 3 Assignment Page</h2>
+		<main className='p-4 max-w-xl mx-auto'>
+			<header className="text-2xl font-bold mb-3">
+				<h1 className='text-xl'>Shopping List</h1>
+			</header>
 
 			<ItemList itemList={items} />
 		</main>
