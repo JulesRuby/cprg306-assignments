80703dd54c1cce439b7a6a149c33b647cf651f75
JulesRuby <JulesRuby@users.noreply.github.com>
2025-09-28 23:43:40 -0600

    Added in styling

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,9 @@
 function Item({ name, quantity, category }) {
 	return (
-		<li className='flex flex-col gap-1 p-[0.5rem] border-b'>
-			<span className='text-bold'>{name}</span>
-			<span className='text-italics'>{category}</span>
-			<span>{quantity > 0 ? `In stock: ${quantity}` : `Out of stock`}</span>
+		<li className='flex flex-col gap-1 p-[0.5rem] border rounded-md capitalize mb-2'>
+			<p className='text-ld'>{name}</p>
+			<p className='text-italics'>{category}</p>
+			<p className="">{quantity > 0 ? `In stock: ${quantity}` : `Out of stock`}</p>
 		</li>
 	);
 }
