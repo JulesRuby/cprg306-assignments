37f029ba157496933972dd02953090529ee87257
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-02 16:38:00 -0600

    added in increment/decrement fns. Added in interactive display.

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -5,8 +5,27 @@ import { useState } from 'react';
 function NewItem() {
   const [quantity, setQuantity] = useState(1);
 
+  const increment = () => {
+    if (quantity <= 20) setQuantity(quantity++);
+  }
+  const decrement = () => {
+    if (quantity >= 1) setQuantity(quantity--);
+  }
 
-	return <div>NewItem</div>;
+	return (
+  <article className="counter-card">
+    <header className="card-header">
+      <h2 className="text-lg font-bold mb-2">New Item</h2>
+    </header>
+
+    <p className="quantity-display">Quantity: {quantity}</p>
+
+    <div className="button-group flex-row gap-2">
+      <button className="dec-btn" onClick={decrement}>-</button>
+      <button className="inc-btn" onClick={increment}>+</button>
+    </div>
+  </article>
+);
 }
 
 export default NewItem;
