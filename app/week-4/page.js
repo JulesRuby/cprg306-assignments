06d81e47a46b40d03ed51e357433ec862f18a3da
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-02 16:15:44 -0600

    Added in week-4\page.js base

---
+++ app/week-4/page.js
@@ -0,0 +1,12 @@
+function Page() {
+	return (
+		<main className='p-4 max-w-xl mx-auto'>
+			<header className="text-2xl font-bold mb-3">
+				<h1 className='text-xl'>Shopping List</h1>
+			</header>
+
+		</main>
+	);
+}
+
+export default Page;
