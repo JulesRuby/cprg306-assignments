b32b868d9c4362fefe8838cd61fd481f9368011b
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-02 16:24:11 -0600

    set component to client render, added in quantity state

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,8 +1,12 @@
+'use client';
+
 import { useState } from 'react';
 
 function NewItem() {
+  const [quantity, setQuantity] = useState(1);
 
-  return <div>NewItem</div>;
+
+	return <div>NewItem</div>;
 }
 
 export default NewItem;
