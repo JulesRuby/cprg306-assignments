dcffaafa99bbd6c63bf6818a4a9fe60250bc2477
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-03 21:48:43 -0600

    So many cumbersome tailwind classes added

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -3,29 +3,41 @@
 import { useState } from 'react';
 
 function NewItem() {
-  const [quantity, setQuantity] = useState(1);
+	const [quantity, setQuantity] = useState(1);
 
-  const increment = () => {
-    if (quantity <= 20) setQuantity(quantity++);
-  }
-  const decrement = () => {
-    if (quantity >= 1) setQuantity(quantity--);
-  }
+	const increment = () => {
+		if (quantity < 20) setQuantity(quantity + 1);
+	};
+	const decrement = () => {
+		if (quantity > 1) setQuantity(quantity - 1);
+	};
 
 	return (
-  <article className="counter-card">
-    <header className="card-header">
-      <h2 className="text-lg font-bold mb-2">New Item</h2>
-    </header>
+		<article className='bg-gray-900 flex flex-col h-[20rem] justify-between p-5 rounded-md text-center w-sm'>
+			<header className='card-header'>
+				<h2 className='text-lg font-bold mb-2'>The Best Item Ever</h2>
+			</header>
 
-    <p className="quantity-display">Quantity: {quantity}</p>
+			<p className='text-xl'>Quantity: {quantity}</p>
 
-    <div className="button-group flex-row gap-2">
-      <button className="dec-btn" onClick={decrement}>-</button>
-      <button className="inc-btn" onClick={increment}>+</button>
-    </div>
-  </article>
-);
+			<div className='button-group flex justify-around gap-4 py-2 text-black text-3xl'>
+				<button
+					className='bg-orange-300 cursor-pointer disabled:bg-gray-400/50 disabled:cursor-not-allowed hover:bg-green-300 font-bold p-3 rounded-[50%] size-[4rem] transition duration-200 ease-in-out active:bg-green-600'
+					onClick={decrement}
+					disabled={quantity <= 1}
+				>
+					-
+				</button>
+				<button
+					className='bg-orange-300 cursor-pointer disabled:bg-gray-400/50 disabled:cursor-not-allowed pointer hover:bg-green-300 font-bold p-3 rounded-[50%] size-[4rem] transition duration-200 ease-in-out active:bg-green-600'
+					onClick={increment}
+					disabled={quantity >= 20}
+				>
+					+
+				</button>
+			</div>
+		</article>
+	);
 }
 
 export default NewItem;
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,10 +1,23 @@
+import NewItem from './new-item';
+import { createPageMetadata } from '../../lib/metadata.js';
+
+// using next's generateMetadata function to create dynamic metadata for the page
+export async function generateMetadata() {
+	return createPageMetadata({
+		title: 'Interactive Counter',
+		description:
+			'Playing around with state to make an interactive stateful presentational component.',
+	});
+}
+
 function Page() {
 	return (
-		<main className='p-4 max-w-xl mx-auto'>
-			<header className="text-2xl font-bold mb-3">
-				<h1 className='text-xl'>Shopping List</h1>
+		<main className='flex flex-col h-screen items-center justify-items-center justify-content-center p-4 max-w-2/3 mx-auto'>
+			<header className=' h-24 flex column text-2xl text-center font-bold mb-3'>
+				<h1 className='text-xl m-auto'>Week-4 : New Item</h1>
 			</header>
 
+			<NewItem />
 		</main>
 	);
 }
