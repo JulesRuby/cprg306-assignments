d9d8d1dbeefbaa2874bff48aab8675b008549a11
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-20 22:48:09 -0600

    Added week-6 Link

--- app/page.js
+++ app/page.js
@@ -1,37 +1,42 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 // adding comment to trigger deploy
 
 export default function Home() {
-	return (
-		<header>
-			<h1 className='text-xl text-bold'>
-				CPRG 306: Web Development 2 - Assignments
-			</h1>
-			<nav className=''>
-				<ul className='nav-list'>
-					<li>
-						<Link href='/week-2' className='text-cyan-400'>
-							Week 2 - Assignment Page
-						</Link>
-					</li>
-					<li>
-						<Link href='/week-3' className='text-cyan-400'>
-							Week 3 - Assignment Page
-						</Link>
-					</li>
-					<li>
-						<Link href='/week-4' className='text-cyan-400'>
-							Week 4 - Assignment Page
-						</Link>
-					</li>
-					<li>
-						<Link href='/week-5' className='text-cyan-400'>
-							Week 5 - Assignment Page
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	);
+  return (
+    <header>
+      <h1 className="text-bold text-xl">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <nav className="">
+        <ul className="nav-list">
+          <li>
+            <Link href="/week-2" className="text-cyan-400">
+              Week 2 - Assignment Page
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-3" className="text-cyan-400">
+              Week 3 - Assignment Page
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-4" className="text-cyan-400">
+              Week 4 - Assignment Page
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-5" className="text-cyan-400">
+              Week 5 - Assignment Page
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-6" className="text-cyan-400">
+              Week 6 - Assignment Page
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
