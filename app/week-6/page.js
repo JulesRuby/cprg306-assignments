03f3b434aa901e32d788ea4c419757cdb62aab05
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-20 23:02:52 -0600

    Copied over week-3 page content and changed metadata slightly

---
+++ app/week-6/page.js
@@ -0,0 +1,27 @@
+import items from "./items.json";
+import ItemList from "./item-list.js";
+// for metadata for page title and description
+import { createPageMetadata } from "../../lib/metadata.js";
+
+// using next's generateMetadata function to create dynamic metadata for the page
+export async function generateMetadata() {
+  return createPageMetadata({
+    title: "Shopping List Part 2",
+    description:
+      "Practicing components and props even more by creating a shopping list app.",
+  });
+}
+
+function Page() {
+  return (
+    <main className="mx-auto max-w-xl p-4">
+      <header className="mb-3 text-2xl font-bold">
+        <h1 className="text-xl">Shopping List</h1>
+      </header>
+
+      <ItemList itemList={items} />
+    </main>
+  );
+}
+
+export default Page;
