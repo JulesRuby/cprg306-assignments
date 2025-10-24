f8fc212194fed1e6f125640eb1949fc28b616afc
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-23 21:34:32 -0600

    Added in a bunch of custom css to the global.css to have some fun

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,18 +1,18 @@
-import Item from './item';
+import Item from "./item";
 
 function ItemList({ itemList }) {
-	return (
-		<ul className='flex flex-col overflow-hidden'>
-            {itemList.map(({ name, category, quantity }, idx) => (
-                <Item
-                    key={idx}
-                    name={name}
-                    category={category}
-                    quantity={quantity}
-                />
-            ))}
-        </ul>
-	);
+  return (
+    <ul className="flex flex-col overflow-hidden">
+      {itemList.map(({ name, category, quantity }, idx) => (
+          <Item
+              key={idx}
+              name={name}
+              category={category}
+              quantity={quantity}
+          />
+      ))}
+    </ul>
+  );
 }
 
 export default ItemList;
--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,11 +1,13 @@
 function Item({ name, quantity, category }) {
-	return (
-		<li className='flex flex-col gap-1 p-[0.5rem] border rounded-md capitalize mb-2'>
-			<p className='text-ld'>{name}</p>
-			<p className='text-italics'>{category}</p>
-			<p className="">{quantity > 0 ? `In stock: ${quantity}` : `Out of stock`}</p>
-		</li>
-	);
+  return (
+    <li className="blur-pane scanlines terminal-gradient-background relative mb-2 flex flex-col gap-1 rounded-md border p-[0.5rem] capitalize">
+      <p className="text-ld">{name}</p>
+      <p className="text-italics">{category}</p>
+      <p className="">
+        {quantity > 0 ? `In stock: ${quantity}` : `Out of stock`}
+      </p>
+    </li>
+  );
 }
 
 export default Item;
