1793a90223b094b5aa402c331a97fca47022897c
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-25 18:24:03 -0600

    Undoing my drunken madness

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,30 +1,37 @@
+"use client";
+
 import itemsData from "./items.json";
 import ItemList from "./item-list.js";
-import useState from "react";
+import { useState } from "react";
 // for metadata for page title and description
-import { createPageMetadata } from "../../lib/metadata.js";
+// import { createPageMetadata } from "../../lib/metadata.js";
 
-const [items, setItems] = useState(itemsData);
+console.log(typeof itemsData);
+console.log(Array.isArray(itemsData));
+console.log({ itemsData });
 
-const handleAddItem = (newItem) => [...items, newItem];
+itemsData.forEach((item) => console.log(item));
 
 // using next's generateMetadata function to create dynamic metadata for the page
-export async function generateMetadata() {
-  return createPageMetadata({
-    title: "WEEEEEOOOOO",
-    description:
-      "Practicing components and props even more by creating a shopping list app.",
-  });
-}
+// export async function generateMetadata() {
+//   return createPageMetadata({
+//     title: "WEEEEEOOOOO",
+//     description:
+//       "Practicing components and props even more by creating a shopping list app.",
+//   });
+// }
 
 function Page() {
+  const [items, setItems] = useState(itemsData);
+  const handleAddItem = (newItem) => [...items, newItem];
+
   return (
     <main className="mx-auto max-w-xl p-4">
       <header className="mb-3 text-2xl font-bold">
         <h1 className="text-xl">Shopping List</h1>
       </header>
 
-      <ItemList itemList={items} onitemAdd={} />
+      <ItemList itemList={items} />
     </main>
   );
 }
