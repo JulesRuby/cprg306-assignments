d0777b89e3c19da11bc56e3aecc4e4d08442e05e
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-25 18:44:00 -0600

    Added the actual item.js for real this time

---
+++ app/week-7/item.js
@@ -0,0 +1,13 @@
+function Item({ name, quantity, category }) {
+  return (
+    <li className="blur-pane terminal-gradient-background scanlines relative mb-2 flex flex-col gap-1 rounded-md border p-[0.5rem] capitalize">
+      <p className="text-ld">{name}</p>
+      <p className="text-italics">{category}</p>
+      <p className="">
+        {quantity > 0 ? `In stock: ${quantity}` : `Out of stock`}
+      </p>
+    </li>
+  );
+}
+
+export default Item;
