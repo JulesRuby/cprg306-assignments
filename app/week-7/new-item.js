c66abc47ceb77de938f578a43da43332a56d8ad1
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-25 18:46:19 -0600

    Added in the actual new-item thing

--- app/week-7/new-item.js
+++ app/week-7/new-item.js
@@ -1,13 +1,41 @@
-function Item({ name, quantity, category }) {
+"use client";
+
+import { useState } from "react";
+
+function NewItem() {
+  const [quantity, setQuantity] = useState(1);
+
+  const increment = () => {
+    if (quantity < 20) setQuantity(quantity + 1);
+  };
+  const decrement = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
+
   return (
-    <li className="relative terminal-gradient-background scanlines mb-2 flex flex-col gap-1 rounded-md border p-[0.5rem] capitalize">
-      <p className="text-ld">{name}</p>
-      <p className="text-italics">{category}</p>
-      <p className="">
-        {quantity > 0 ? `In stock: ${quantity}` : `Out of stock`}
-      </p>
-    </li>
+    <article className="flex min-h-[10rem] w-sm flex-col justify-between rounded-md bg-gray-900 p-5 text-center">
+      <p className="text-xl">Quantity: {quantity}</p>
+
+      <div className="button-group flex justify-around gap-4 py-2 text-3xl text-black">
+        <button
+          type="button"
+          className="h-[3rem] w-[4rem] cursor-pointer rounded-[10%] bg-orange-300 p-1 align-middle font-bold transition duration-200 ease-in-out hover:bg-green-300 active:bg-green-600 disabled:cursor-not-allowed disabled:bg-gray-400/50"
+          onClick={decrement}
+          disabled={quantity <= 1}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          className="pointer size-[2rem] h-[3rem] w-[4rem] cursor-pointer rounded-[10%] bg-orange-300 p-1 font-bold transition duration-200 ease-in-out hover:bg-green-300 active:bg-green-600 disabled:cursor-not-allowed disabled:bg-gray-400/50"
+          onClick={increment}
+          disabled={quantity >= 20}
+        >
+          +
+        </button>
+      </div>
+    </article>
   );
 }
 
-export default Item;
+export default NewItem;
