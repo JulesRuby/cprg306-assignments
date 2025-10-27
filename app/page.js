476946b15b4731ffa96f89c3c5cf998899ec3e18
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-27 04:42:03 -0600

    Implemented programmatic Link generation in nav

--- app/page.js
+++ app/page.js
@@ -1,45 +1,55 @@
-import Link from "next/link";
+// "use client";
 
-// adding comment to trigger deploy
+import Link from "next/link";
+import getAllRoutes from "@/lib/routes";
 
 export default function Home() {
+  const routes = getAllRoutes();
+
   return (
     <header>
       <h1 className="text-bold text-xl">
         CPRG 306: Web Development 2 - Assignments
       </h1>
-      <nav className="">
+      <nav className="nav-shudder">
         <ul className="nav-list">
-          <li>
-            <Link href="/week-2" className="text-cyan-400">
+          {/* <li>
+            <Link href="/week-2" className="">
               Week 2 - Assignment Page
             </Link>
           </li>
           <li>
-            <Link href="/week-3" className="text-cyan-400">
+            <Link href="/week-3" className="">
               Week 3 - Assignment Page
             </Link>
           </li>
           <li>
-            <Link href="/week-4" className="text-cyan-400">
+            <Link href="/week-4" className="">
               Week 4 - Assignment Page
             </Link>
           </li>
           <li>
-            <Link href="/week-5" className="text-cyan-400">
+            <Link href="/week-5" className="">
               Week 5 - Assignment Page
             </Link>
           </li>
           <li>
-            <Link href="/week-6" className="text-cyan-400">
+            <Link href="/week-6" className="">
               Week 6 - Assignment Page
             </Link>
           </li>
           <li>
-            <Link href="/week-7" className="text-cyan-400">
+            <Link href="/week-7" className="">
               Week 7 - Assignment Page
             </Link>
-          </li>
+          </li> */}
+          {routes.map((route) => (
+            <li key={route.path}>
+              <Link href={route} className="">
+                {`${route.replace("-", " ").replace(/^\/w/, "W")} - Assignment Page`}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
