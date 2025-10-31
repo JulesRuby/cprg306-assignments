0d38c72ced9057f1d1b35ee7b7527a79a6ab84ca
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-31 14:56:53 -0600

    Added in page.js week 9

---
+++ app/week-9/page.js
@@ -0,0 +1,16 @@
+import CounterDemoSection from './CounterDemoSection';
+
+function Page() {
+
+  return (
+    <main className="mx-auto max-w-xl p-4">
+      <header className="mb-3 text-2xl font-bold">
+        <h1 className="text-xl">Week 9</h1>
+      </header>
+
+      <CounterDemoSection />
+    </main>
+  );
+}
+
+export default Page;
