965e9b81aee4255d532cf713db3aa25fb0b64951
JulesRuby <JulesRuby@users.noreply.github.com>
2025-11-25 00:06:30 -0700

    Changed route.js logic to avoid displaying weird Links

--- app/page.js
+++ app/page.js
@@ -1,18 +1,40 @@
 // "use client";
+import { useMemo } from "react";
 
 import Link from "next/link";
 import getAllRoutes from "@/lib/routes";
 
 export default function Home() {
-  const routes = getAllRoutes();
+  // const routes = getAllRoutes();
+
+  const routes = getAllRoutes({ includeNested: false, maxDepth: 0 });
   // swap any of the routes not beginning with /week- to the end end of the array
-  const sortedRoutes = [...routes].sort((a, b) => {
-    const aIsWeek = a.startsWith("/week-");
-    const bIsWeek = b.startsWith("/week-");
-    if (aIsWeek && !bIsWeek) return -1;
-    if (!aIsWeek && bIsWeek) return 1;
-    return 0;
-  });
+  // const sortedRoutes = [...routes].sort((a, b) => {
+  //   const aIsWeek = a.startsWith("/week-");
+  //   const bIsWeek = b.startsWith("/week-");
+  //   if (aIsWeek && !bIsWeek) return -1;
+  //   if (!aIsWeek && bIsWeek) return 1;
+  //   return 0;
+  // });
+
+  const sortMattedRoutes = useMemo(
+    () =>
+      routes.map((route) => ({
+        path: route,
+        displayName: route
+          .replace(/^\//, "")
+          .replace(/-/g, " ")
+          .split("/")
+          .map((part) =>
+            part
+              .split(" ")
+              .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(" "),
+          )
+          .join("/"),
+      })),
+    [routes],
+  );
 
   return (
     <header>
@@ -21,40 +43,10 @@ export default function Home() {
       </h1>
       <nav className="nav-shudder">
         <ul className="nav-list">
-          {/* <li>
-            <Link href="/week-2" className="">
-              Week 2 - Assignment Page
-            </Link>
-          </li>
-          <li>
-            <Link href="/week-3" className="">
-              Week 3 - Assignment Page
-            </Link>
-          </li>
-          <li>
-            <Link href="/week-4" className="">
-              Week 4 - Assignment Page
-            </Link>
-          </li>
-          <li>
-            <Link href="/week-5" className="">
-              Week 5 - Assignment Page
-            </Link>
-          </li>
-          <li>
-            <Link href="/week-6" className="">
-              Week 6 - Assignment Page
-            </Link>
-          </li>
-          <li>
-            <Link href="/week-7" className="">
-              Week 7 - Assignment Page
-            </Link>
-          </li> */}
-          {sortedRoutes.map((route, idx) => (
+          {sortMattedRoutes.map((route, idx) => (
             <li key={`${route.path} - ${idx}`}>
               <Link href={route} className="capitalize">
-                {`${route.replace("-", " ").replace(/^\//, "")} - Assignment Page`}
+                {`${route.displayName} - Assignment Page`}
               </Link>
             </li>
           ))}
